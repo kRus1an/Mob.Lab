027cc16fe23936ebc2a657f22c21a3293cfc3b62
Ruslan <karimov_r@iuca.kg>
2024-11-20 21:23:00 +0600

    this is my lab 2

--- app/src/main/java/com/example/MyFirstApp/MainActivity.kt
+++ app/src/main/java/com/example/MyFirstApp/MainActivity.kt
@@ -1,16 +1,40 @@
+package kg.iuca.MyFirstApp
+
+
+
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.webkit.WebSettings.TextSize
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.material3.darkColorScheme
+import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.em
 import com.example.MyFirstApp.ui.theme.MyFirstAppTheme
 
 @SuppressLint("RestrictedApi")
@@ -24,32 +48,86 @@ class MainActivity : ComponentActivity() { // Исправлено: исполь
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Task1And2()
+                    Task4And3()
                 }
             }
         }
     }
 
-    @Composable
-    fun Task1And2() {
-        val countNum = remember { mutableStateOf(0) }
-        val helloWorld = remember { mutableStateOf("Привет, мир!") }
 
-        Column {
-            Text(text = helloWorld.value)
-            Button(onClick = {
-                helloWorld.value = "Кнопка нажата"
-            }) {
-                Text("Button")
-                Text("Нажми на меня")
-            }
+@Composable
+fun Task4And3() {
+// This is task 4
+    var isDarkTheme by remember { mutableStateOf(false) }
 
-            Text(text = "Counting number: ${countNum.value}")
-            Button(onClick = {
-                countNum.value += 1
-            }) {
-                Text("Я счетчик нажатий")
+    MaterialTheme(
+        colorScheme = if (isDarkTheme) darkColorScheme() else lightColorScheme()
+    ) {
+        Surface(
+            modifier = Modifier.fillMaxSize(),
+            color = MaterialTheme.colorScheme.background
+        ) {
+            Column(modifier = Modifier.padding(16.dp)) {
+
+                Text(
+                    text = "Приложения Список Задач",
+                    fontSize = 5.em
+                )
+                Spacer(modifier = Modifier.height(30.dp))
+                Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.SpaceBetween) {
+                    Text(
+                        text = if (isDarkTheme) "Темная тема" else "Светлая тема",
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                    Switch(
+                        checked = isDarkTheme,
+                        onCheckedChange = { isDarkTheme = it }
+                    )
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+
+
+                Task3()
             }
         }
+    }
+}
+@Composable
+fun Task3() {
+// This is task 3
+    var taskText by remember { mutableStateOf("") }
+    var tasks by remember { mutableStateOf(listOf<String>()) }
+    Column (modifier = Modifier.padding(16.dp)){
+        TextField(
+            value = taskText,
+            onValueChange = {taskText = it},
+            label = { Text(text = "Введите задачу")},
+            modifier = Modifier.fillMaxWidth(),
+            colors = TextFieldDefaults.colors(
+                focusedContainerColor = MaterialTheme.colorScheme.surface,
+                unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+                focusedTextColor = MaterialTheme.colorScheme.onBackground,
+                unfocusedTextColor = MaterialTheme.colorScheme.onBackground
+            )
+        )
+        Spacer(modifier = Modifier.height(8.dp))
+        Button(
+            onClick = {
+                if (taskText.isNotEmpty()){
+                    tasks = tasks + taskText
+                    taskText = " "
+                }
+            }, modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Добавить задачу")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        LazyColumn {
+            items(tasks){ task ->
+                Text(text = task, modifier = Modifier.padding(4.dp))
+            }
+        }
+
+
     }
 }
